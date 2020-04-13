fdad11b0d058139cfb338f6e8ad2b258319c75e8
pierre.bouteloup <p.bouteloup@b-p.fr>
2020-04-13 17:25:04 +0200

    wip: add basic box2D template

--- main.lua
+++ main.lua
@@ -1,3 +1,37 @@
-local ffi = require('ffi')
+local WORLD = love.physics.newWorld()
+local BODY = love.physics.newBody(WORLD, 0, 0, 'dynamic')
+local SHAPE = love.physics.newRectangleShape(100, 50)
+local FIXTURE = love.physics.newFixture(BODY, SHAPE, 1)
+local JOINT = love.physics.newMouseJoint(BODY, 0, 0)
+JOINT:setMaxForce(3000000)
 
-local test = ffi.load('clipper-binding.dll')
+local bodies, shapes, fixtures = {}, {}, {}
+
+local RAND = love.math.newRandomGenerator()
+
+local W, H = love.graphics.getDimensions()
+
+for i = 1, 10 do
+  local body = love.physics.newBody(WORLD, RAND:random(0, W), RAND:random(0, H))
+  local shape = love.physics.newRectangleShape(RAND:random(10, 50), RAND:random(10, 50))
+  local fixture = love.physics.newFixture(body, shape, 1)
+  table.insert(bodies, body)
+  table.insert(shapes, shape)
+  table.insert(fixtures, fixture)
+end
+
+local X, Y = 0, 0
+
+function love.update(dt)
+  JOINT:setTarget(love.mouse.getPosition())
+  WORLD:update(dt)
+end
+
+function love.draw()
+  love.graphics.setColor(1, 0, 0)
+  for i, fixture in ipairs(fixtures) do
+    love.graphics.polygon('line', fixture:getBody():getWorldPoints(fixture:getShape():getPoints()))
+  end
+  love.graphics.setColor(0, 1, 0)
+  love.graphics.polygon('fill', BODY:getWorldPoints(SHAPE:getPoints()))
+end
